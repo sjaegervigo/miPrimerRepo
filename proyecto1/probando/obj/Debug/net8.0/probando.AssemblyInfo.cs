4acdba2f888a212858c7cee8b04cb536bab7ddb2
Sebastian Jaeger <sjaegervigo@gmail.com>
2025-04-08 15:25:43 -0500

    nuevo gitignored creado

--- proyecto1/probando/obj/Debug/net8.0/probando.AssemblyInfo.cs
+++ proyecto1/probando/obj/Debug/net8.0/probando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("probando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3044cef3bec8f46d498fb0b53bdc91b15670731")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+988abc401ad6e37a290cb123071758d33bbbb7eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("probando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("probando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
