772e2ba3aefe974617f375bc8d18d1f31244df96
Sebastian Jaeger Vigo <95112798+sjaegervigo@users.noreply.github.com>
2025-04-09 21:24:33 -0500

    Update Program.cs deleting secrets

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,6 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Platzi!");
 
-string stripe_api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
-
-string testingConnection = "Server=tcp:aminesserver.database.windows.net,1433;Initial";
