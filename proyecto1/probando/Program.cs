03bac7c09c93f4f2d55ec67798f53113ffb7848b
Sebastian Jaeger <sjaegervigo@gmail.com>
2025-04-09 21:15:25 -0500

    cadena nueva

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, Platzi!");
 
 string stripe_api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
+
+string testingConnection = "Server=tcp:aminesserver.database.windows.net,1433;Initial";
